0fb1bd8cbb1915f92f3a5a22f38083e14a47d6f8
Aquapunktura <aquapunktura@ya.ru>
2022-06-23 00:09:03 +0300

    alphabet

--- VUE/vueproject/src/ostov-test/test.vue
+++ VUE/vueproject/src/ostov-test/test.vue
@@ -16,68 +16,23 @@
         </div>
       </div>
       <div class="alphabetButtons">
-        <div class="vowels">
-          <div class="vow1">
-            <div>А</div>
-            <div>О</div>
-            <div>У</div>
-            <div>Э</div>
-            <div>Ы</div>
-          </div>
-          <div class="vow2">
-            <div>Я</div>
-            <div>Ё</div>
-            <div>Ю</div>
-            <div>Е</div>
-            <div>И</div>
-          </div>
+        <div class="groupA">
+          a
         </div>
-        <div class="consonants">
-          <div class="con1">
-            <div>Б</div>
-            <div>Д</div>
-            <div>Г</div>
-          </div>
-          <div class="con2">
-            <div>П</div>
-            <div>Т</div>
-            <div>К</div>
-          </div>
-          <div class="con3">
-            <div>В</div>
-            <div>Ж</div>
-            <div>З</div>
-          </div>
-          <div class="con4">
-            <div>Ф</div>
-            <div>Ш</div>
-            <div>С</div>
-          </div>
-          <div class="con51">
-            <div>Л</div>
-          </div>
-          <div class="con52">
-            <div>М</div>
-          </div>
-          <div class="con53">
-            <div>Н</div>
-          </div>
-          <div class="con54">
-            <div>Р</div>
-          </div>
-          <div class="con6">
-            <div>Й</div>
-            <div>Ц</div>
-            <div>Щ</div>
-          </div>
-          <div class="con7">
-            <div>Х</div>
-            <div>Ч</div>
-          </div>
-          </div>
-        <div class="silent">
-            <div>Ъ</div>
-            <div>Ь</div>
+        <div class="groupB">
+          b
+        </div>
+        <div class="groupC">
+          c
+        </div>
+        <div class="groupD">
+          d
+        </div>
+        <div class="groupE">
+          e
+        </div>
+        <div class="groupF">
+          f
         </div>
       </div>
     </div>
@@ -114,18 +69,18 @@ export default {
   background-color: #2B2633;
   grid-template-rows: 0.2fr 0.1fr auto;
 }
-.main>*{
+.main>* {
   display: grid;
   place-items: center;
 }
-.nameReady{
+.nameReady {
   border-bottom: 1px solid #B4ADA3;
   border-radius: 7px;
   font-size: xx-large;
   color: #B4ADA3;
   padding: 10px 20px;
 }
-.choiceButtons{
+.choiceButtons {
   border-radius: 7px;
   font-size: x-large;
   color: #B4ADA3;
@@ -134,67 +89,42 @@ export default {
   margin-top: 30px;
   gap: 50px;
 }
-.alphabetButtons{
-  display: grid;
-  height: 100vh;
-  border-radius: 7px;
-  font-size: x-large;
-  color: #B4ADA3;
-  padding: 10px 20px;
-  gap: 20px;
-  grid-template-areas:
-  "A B C D E F H I"
-  "A B C D E F H I"
-  "A B C D E F H  "
-  "A B            "
-  "A B G G G G J J";
-}
-.vowels{
+.alphabetButtons {
   display: grid;
   place-items: center;
+  grid-template-areas:
+  "A B C D"
+  "A B C D"
+  "A B C D"
+  "A E E F"
+  "A E E F";
+  width: 100%;
+  height: 100%;
 }
-.vow1 {
+.alphabetButtons>* {
+  border: 1px solid #fff;
+  display: grid;
+  place-items: center;
+  width: 100%;
+  height: 100%;
+}
+.groupA {
   grid-area: A;
 }
-.vow2 {
+.groupB {
   grid-area: B;
 }
-.consonants{
-  display: grid;
-  place-items: center;
-}
-.con1{
+.groupC {
   grid-area: C;
 }
-.con2{
+.groupD {
   grid-area: D;
 }
-.con3{
+.groupE {
   grid-area: E;
 }
-.con4{
+.groupF {
   grid-area: F;
-}
-.con51{
-  grid-area: F;
-}
-.con52{
-  grid-area: F;
-}
-.con53{
-  grid-area: F;
-}
-.con54{
-  grid-area: F;
-}
-.con6{
-  grid-area: F;
-}
-.con7{
-  grid-area: F;
-}
-.silent {
-  grid-area: ;
 }
 .menu {
   display: grid;
